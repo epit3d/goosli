325c8eed4ed3cee8b7bd6a80870c64e2c17c5243
lelami <lelaminova@gmail.com>
2020-07-06 14:22:21 +0300

    add tests to line, plane, triangle

--- primitives/test/line_test.go
+++ primitives/test/line_test.go
@@ -87,3 +87,92 @@ func TestLine_IsIntersectingSegment(t *testing.T) {
 		})
 	}
 }
+
+func TestLine_IntersectLine(t *testing.T) {
+
+	cases := []struct {
+		in1 Line
+		in2 Line
+		out *Point
+	}{
+		// Lines intersect on XY plane
+		{
+			in1: Line{Point{3, 2, 0}, Point{8, 7, 0}},
+			in2: Line{Point{1, 6, 0}, Point{7, 3, 0}},
+			out: &Point{5, 4, 0},
+		},
+		// Lines intersect on YZ plane - fail
+/*		{
+			in1: Line{Point{0, 3, 2}, Point{0, 8, 7}},
+			in2: Line{Point{0, 1, 6}, Point{0, 7, 3}},
+			out: &Point{0, 5, 4},
+		},*/
+
+		// Lines intersect on XZ plane - fail
+/*		{
+			in1: Line{Point{3, 0, 2}, Point{8, 0, 7}},
+			in2: Line{Point{1, 0, 6}, Point{7, 0, 3}},
+			out: &Point{5, 0, 4},
+		},*/
+		// Lines intersect
+		{
+			in1: Line{Point{1, 1, -2}, Point{1, -5, 1}},
+			in2: Line{Point{1, -3, 0}, Point{-1, 0, -4}},
+			out: &Point{1, -3, 0},
+		},
+		// 1, 2, 3 points collinear, 3 lies b/w 1,2
+		{
+			in1: Line{Point{1, -3, 0}, Point{-1, 0, -4}},
+			in2: Line{Point{0, -1.5, -2}, Point{0, 0, 0}},
+			out: &Point{0, -1.5, -2},
+		},
+		//  1, 2, 3 points collinear, 3 lies b/w 1,2
+		{
+			in1: Line{Point{1, -3, 0}, Point{-3, 3, -8}},
+			in2: Line{Point{-1, 0, -4}, Point{0, 0, 0}},
+			out: &Point{-1, 0, -4},
+		},
+		// not intersect
+		{
+			in1: Line{Point{1, -3, 0}, Point{-3, 3, -8}},
+			in2: Line{Point{1, 0, 4}, Point{0, 0, 0}},
+			out: nil,
+		},
+
+	}
+
+	for i, row := range cases {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			require.Equal(t, row.out, row.in1.IntersectLine(&row.in2))
+		})
+	}
+}
+
+func TestLine_IntersectTriangle(t *testing.T) {
+
+	cases := []struct {
+		in1 Line
+		in2 Triangle
+		out bool
+	}{
+		//Line intersect one side of triangle
+		{
+			in1: Line{Point{1, 1, -2}, Point{1, -5, 1}},
+			in2: NewTriangle(Point{1, -3, 0}, Point{-1, 0, -4}, Point{0, 0, 0}),
+			out: true,
+		},
+		//Not intersect
+		{
+			in1: Line{Point{10, 10, 10}, Point{9, 9, 9}},
+			in2: NewTriangle(Point{1, -3, 0}, Point{-1, 0, -4}, Point{0, 0, 0}),
+			out: false,
+		},
+
+	}
+
+	for i, row := range cases {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			require.Equal(t, row.out, row.in1.IntersectTriangle(row.in2))
+		})
+	}
+}
--- primitives/test/triangle_test.go
+++ primitives/test/triangle_test.go
@@ -30,3 +30,115 @@ func TestTriangle_MinZ(t *testing.T) {
 		})
 	}
 }
+
+func TestTriangle_IntersectTriangle(t *testing.T) {
+		cases := []struct {
+		in1 Triangle
+		in2 Triangle
+		out *Line
+	}{
+		//line inside of triangle
+		{
+			in1: NewTriangle(Point{0, 0, 5}, Point{0, 10, 5}, Point{10, 0, 5}),
+			in2: NewTriangle(Point{2, 0, 0}, Point{2, 7, 0}, Point{2, 0, 7}),
+			out: &Line{Point{2,0,5}, Point{2,2,5}},
+		},
+		//line intersect one side of triangle
+		{
+			in1: NewTriangle(Point{0, 0, 5}, Point{0, 10, 5}, Point{10, 0, 5}),
+			in2: NewTriangle(Point{2, -1, 0}, Point{2, 7, 0}, Point{2, -1, 8}),
+			out: &Line{Point{2,0,5}, Point{2,2,5}},
+		},
+		{
+			in1: NewTriangle(Point{0, 10, 5}, Point{0, 0, 5}, Point{10, 0, 5}),
+			in2: NewTriangle(Point{2, -1, 0}, Point{2, 7, 0}, Point{2, -1, 8}),
+			out: &Line{Point{2,2,5}, Point{2,0,5}},
+		},
+		{
+			in1: NewTriangle(Point{0, 0, 5}, Point{10, 0, 5}, Point{0, 10, 5}),
+			in2: NewTriangle(Point{2, -1, 0}, Point{2, 7, 0}, Point{2, -1, 8}),
+			out: &Line{Point{2,2,5}, Point{2,0,5}},
+		},
+		//line intersect two sides of triangle
+		{
+			in1: NewTriangle(Point{0, 0, 5}, Point{10, 0, 5}, Point{0, 10, 5}),
+			in2: NewTriangle(Point{9, -1, 0}, Point{9, 7, 0}, Point{9, -1, 8}),
+			out: &Line{Point{9,0,5}, Point{9,1,5}},
+		},
+		{
+			in1: NewTriangle(Point{0, 0, 5}, Point{0, 10, 5}, Point{10, 0, 5}),
+			in2: NewTriangle(Point{9, -1, 0}, Point{9, 7, 0}, Point{9, -1, 8}),
+			out: &Line{Point{9,0,5}, Point{9,1,5}},
+		},
+		{
+			in1: NewTriangle(Point{10, 0, 5}, Point{0, 0, 5}, Point{0, 10, 5}),
+			in2: NewTriangle(Point{9, -1, 0}, Point{9, 7, 0}, Point{9, -1, 8}),
+			out: &Line{Point{9,0,5}, Point{9,1,5}},
+		},
+		//line intersect one side and one vertex
+		{
+			in1: NewTriangle(Point{8, 0, 5}, Point{10, 0, 5}, Point{9, 1, 5}),
+			in2: NewTriangle(Point{9, -1, 0}, Point{9, 7, 0}, Point{9, -1, 8}),
+			out: &Line{Point{9,0,5}, Point{9,1,5}},
+		},
+		// not intersect
+		{
+			in1: NewTriangle(Point{2, 0, 5}, Point{0, 0, 5}, Point{0, 2, 5}),
+			in2: NewTriangle(Point{9, -1, 0}, Point{9, 7, 0}, Point{9, -1, 8}),
+			out: nil,
+		},
+	}
+	for i, row := range cases {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			require.Equal(t, row.out, row.in1.IntersectTriangle(&row.in2))
+		})
+	}
+}
+
+func TestTriangle_PointBelongs(t *testing.T) {
+	cases := []struct {
+		in1 Triangle
+		in2 Point
+		out bool
+	}{
+		//on XY plane
+		{
+			in1: NewTriangle(Point{10, 0, 5}, Point{0, 0, 5}, Point{0, 10, 5}),
+			in2: Point{3,4,5},
+			out: true,
+		},
+		{
+			in1: NewTriangle(Point{10, 0, 5}, Point{0, 0, 5}, Point{0, 10, 5}),
+			in2: Point{3,4,0},
+			out: false,
+		},
+		//on YZ plane
+		{
+			in1: NewTriangle(Point{2, 0, 0}, Point{2, 7, 0}, Point{2, 0, 7}),
+			in2: Point{2,1,1},
+			out: true,
+		},
+		//on XZ plane
+		{
+			in1: NewTriangle(Point{0, 2, 0}, Point{7, 2, 0}, Point{0, 2, 7}),
+			in2: Point{1,2,1},
+			out: true,
+		},
+		//arbitrary plane
+		{
+			in1: NewTriangle(Point{2, 2, 2}, Point{4, 1, 1}, Point{6, 2, -2}),
+			in2: Point{3,2,1},
+			out: true,
+		},
+		{
+			in1: NewTriangle(Point{2, 2, 2}, Point{4, 1, 1}, Point{6, 2, -2}),
+			in2: Point{10,-10,6},
+			out: false,
+		},
+	}
+	for i, row := range cases {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			require.Equal(t, row.out, row.in1.PointBelongs(row.in2))
+		})
+	}
+}
